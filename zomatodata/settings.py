554bc2ffa22d6e055e211ad052b23678fab65920
Prashanth <prashanth9c007@gmail.com>
2017-01-16 13:31:24 +0530

    TK scraper

---
+++ zomatodata/settings.py
@@ -0,0 +1,5 @@
+SPIDER_MODULES = ['zomatodata.spiders']
+NEWSPIDER_MODULE = 'zomatodata.spiders'
+DEFAULT_ITEM_CLASS = 'zomatodata.items.Restaurant'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+
